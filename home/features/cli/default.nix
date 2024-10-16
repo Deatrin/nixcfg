42e8828a4bab204ca5d42c1a17787cb5021a92d4
Deatrin <jennexa@gmail.com>
2024-10-16 01:32:01 -0700

    add cli config

---
+++ home/features/cli/default.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  ...
+}: {
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = ["--cmd cd"];
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+    extraOptions = [ "-l" "--icons" "--git" "-a" ];
+  };
+  
+  home.packages = with pkgs; [
+    coreutils
+    fd
+    htop
+    httpie
+    jq
+    procs
+    ripgrep
+    tldr
+    zip
+  ];
+}
