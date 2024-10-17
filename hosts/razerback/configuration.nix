356e123bdb6e4e51ae58b362d3d791930395db7d
Deatrin <jennexa@gmail.com>
2024-10-17 00:03:54 -0700

    Update configuration.nix

--- hosts/razerback/configuration.nix
+++ hosts/razerback/configuration.nix
@@ -86,8 +86,8 @@
     settings.PermitRootLogin = "no";
     allowSFTP = true;
   };
-  
-  hardware.enable.bluetooth.enable = true;
+
+  hardware.bluetooth.enable = true;
 
   programs.hyprland = {
     enable = true;
