4c61479569d6029e8d39a70fb088a850280e1e57
Deatrin <jennexa@gmail.com>
2024-10-16 23:17:57 -0700

    Update secrets.nix

--- hosts/razerback/secrets.nix
+++ hosts/razerback/secrets.nix
@@ -1,9 +1,6 @@
 {
   age = {
     secrets = {
-      secret1 = {
-        file = ../../secrets/secret1.age;
-      };
       deatrin-secrets = {
         file = ../../secrets/deatrin-secrets.age;
         owner = "deatrin";
