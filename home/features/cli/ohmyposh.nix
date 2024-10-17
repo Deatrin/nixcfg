d0a24ba1401b400e629230dec529a58ed80e9374
Deatrin <jennexa@gmail.com>
2024-10-16 18:50:30 -0700

    Update ohmyposh.nix

--- home/features/cli/ohmyposh.nix
+++ home/features/cli/ohmyposh.nix
@@ -1,5 +1,7 @@
 {...}: {
-  enable = true;
-  enableZshIntegration = true;
-  settings = builtins.fromTOML (builtins.unsafeDiscardStringContext (builtins.readFile ./ohmyposh/amro.toml));
+  programs.oh-my-posh = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = builtins.fromTOML (builtins.unsafeDiscardStringContext (builtins.readFile ./ohmyposh/amro.toml));
+  };
 }
