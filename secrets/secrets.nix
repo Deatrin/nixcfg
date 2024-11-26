e2ff8e4dcb7bdef505cdf54c269a8e92b59f7769
Deatrin <jennexa@gmail.com>
2024-11-25 23:48:47 -0800

    Update secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgfBnUK555uIgENo1X0P2hBb7vDQcqDtA3HSPxsf0Sa";
+  razerback = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtfGo0zja7m0xzFgW7Bd7TwcfGmZWrN5HvcUl3F+SJx";
   deatrin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIY06wy+S3ze6CsUzIPQmErBxT4V3FLfAIzANNDEogte jennexa@gmail.com";
 in {
   "deatrin-secrets.age".publicKeys = [razerback deatrin];
